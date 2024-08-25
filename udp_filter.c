67fd4732c2d0958f55818dd17d6c1c7925536594
ruhuang2001 <ruhuang2001@gmail.com>
2024-08-25 23:28:21 +0800

    Add denied_clients for udp_filter to drop unnesscary package

--- udp_filter.c
+++ udp_filter.c
@@ -7,6 +7,7 @@
 #include <bpf/bpf_helpers.h>
 
 #define SECRET_KNOCK "secret" // 定义一个秘密敲门的字符串，用于认证
+#define MAX_FAILURES 3        // 定义最大失败次数
 
 // 定义一个哈希表，用于存储允许的客户端 IP 地址
 struct {
@@ -15,6 +16,14 @@ struct {
     __type(key, __be32);               // 键是 IP 地址（使用网络字节序）
     __type(value, __u32);              // 值是敲门事件计数，设为 1 表示允许
 } allowed_clients SEC(".maps");
+
+// 定义一个哈希表，用于存储拒绝的客户端 IP 地址
+struct {
+    __uint(type, BPF_MAP_TYPE_HASH);   // 定义为哈希类型的 BPF map
+    __uint(max_entries, 1024);         // 最大支持 1024 个条目
+    __type(key, __be32);               // 键是 IP 地址（使用网络字节序）
+    __type(value, __u32);              // 值是失败次数
+} denied_clients SEC(".maps");
 
 // 自定义的 ntohs 函数，用于将 16 位数值从网络字节序转换为主机字节序
 static __u16 my_ntohs(__u16 val) {
@@ -36,6 +45,12 @@ int udp_filter(struct xdp_md *ctx) {
     struct iphdr *ip = (void *)(eth + 1);
     if ((void *)(ip + 1) > data_end)
         return XDP_PASS;    // 如果包长度小于 IP 头部长度，直接通过
+
+    // 检查是否在拒绝列表中
+    __u32 *is_denied = bpf_map_lookup_elem(&denied_clients, &ip->saddr);
+    if (is_denied && *is_denied > MAX_FAILURES) {
+        return XDP_DROP;  // 如果在拒绝列表中，直接丢弃数据包
+    }
 
     // 检查协议
     if (ip->protocol != IPPROTO_UDP)
@@ -67,11 +82,20 @@ int udp_filter(struct xdp_md *ctx) {
         }
     }
 
-    // 匹配后更新哈希表
+    // 匹配后更新允许哈希表
     if (match) {
         __u32 value = 1;
         bpf_map_update_elem(&allowed_clients, &ip->saddr, &value, BPF_ANY);
         return XDP_PASS;
+    } else {
+        // 失败更新拒绝哈希表
+         __u32 *fail_count = bpf_map_lookup_elem(&denied_clients, &ip->saddr);
+        if (fail_count) {
+            *fail_count += 1;
+        } else {
+            __u32 initial_fail_count = 1;
+            bpf_map_update_elem(&denied_clients, &ip->saddr, &initial_fail_count, BPF_ANY);
+        }
     }
 
     return XDP_PASS;
